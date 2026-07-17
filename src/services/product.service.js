78b17c8170b6746ed6e9bec8bf7a7e7002cf35f1
Aawa Hang Limbu <aawahang355@gmail.com>
2026-07-17 20:16:57 +0545

    adsdf

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -10,3 +10,18 @@ export const getProductById = async (id) => {
   const { data } = await api.get(`/products/${id}`);
   return data; // { product }
 };
+
+export const createProduct = async (formData) => {
+  const { data } = await api.post("/products", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return data;
+};
+
+export const updateProduct = async (id, formData) => {
+  const { data } = await api.put(`/products/${id}`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+  return data;
+};
+
