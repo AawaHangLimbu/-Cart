43e9b3329d8f6ee771cba9f92b913f4c7664ff03
Aawa Hang Limbu <aawahang355@gmail.com>
2026-07-21 21:05:26 +0545

    adsfdsa

---
+++ src/products/CategoryFilter.jsx
@@ -0,0 +1,13 @@
+// components/products/CategoryFilter.jsx
+const CATEGORIES = [
+  "All",
+  "Groceries",
+  "Electronics",
+  "Clothing",
+  "Food & Drinks",
+  "Health",
+  "Home",
+  "Sports",
+  "Books",
+];
+
